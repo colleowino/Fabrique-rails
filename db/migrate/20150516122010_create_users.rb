458c7fd047423b4d877b2d5fbdc366ce882562aa
colleowino <colleowino@gmail.com>
2015-05-16 15:23:11 +0300

    added users

---
+++ db/migrate/20150516122010_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :location
+      t.string :avatar_url
+
+      t.timestamps null: false
+    end
+  end
+end
